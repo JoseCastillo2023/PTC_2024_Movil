f02b25597037f3b62680e023bf9995b5c6f4a6e9
edgarcia1723 <20220082@ricaldone.edu.sv>
2024-08-06 17:46:41 -0600

    Cambio de estructura

--- App.js
+++ App.js
@@ -1,4 +1,85 @@
 // Importar Dependencias que se utilizara.
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AuthProvider, useAuth } from './src/contexts/AuthContext';
+import { UserProvider } from './src/contexts/UserContext';
+import StackMain from './src/navigation/StackMain';
+import StackAuth from './src/navigation/StackAuth';
+import SplashScreen from './src/screens/SplashScreen';
+import { SERVER } from './src/contexts/Network';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  const { isLoggedIn, setIsLoggedIn } = useAuth();
+  const [isLoading, setIsLoading] = useState(true); // Estado para manejar la pantalla de carga
+
+  const getSession = async () => {
+    try {
+      const response = await fetch(`${SERVER}services/public/clientes.php?action=getUser`, {
+        method: 'POST',
+      });
+      const data = await response.json();
+      if (data.status) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setIsLoggedIn(false); // Manejo de error: Se asume que la sesión no está iniciada
+    } finally {
+      setIsLoading(false); // Deja de mostrar la pantalla de carga
+    }
+  };
+
+  useEffect(() => {
+    getSession();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <UserProvider>
+        <Stack.Navigator>
+          {isLoading ? (
+            <Stack.Screen
+            name="Splash"
+            component={SplashScreen}
+            options={{ headerShown: false }}
+            initialParams={{ log: isLoading}}
+          />
+          ) : isLoggedIn ? (
+            <Stack.Screen
+              name="Main"
+              component={StackMain}
+              options={{ headerShown: false }}
+            />
+          ) : (
+            <Stack.Screen
+              name="Auth"
+              component={StackAuth}
+              options={{ headerShown: false }}
+            />
+          )}
+        </Stack.Navigator>
+      </UserProvider>
+    </NavigationContainer>
+  );
+};
+
+const AppWrapper = () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  );
+};
+
+export default AppWrapper;
+
+
+/*
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
@@ -100,3 +181,5 @@ export default function App() {
         </NavigationContainer>
     );
 }
+
+*/
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ usuarioAuth }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {usuarioAuth}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
+
+export { AuthProvider, useAuth };

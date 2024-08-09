c31cf3853e9b14c414b6f5a3c22fdd8ffaa45bd0
JoseCastillo2023 <20220235@ricaldone.edu.sv>
2024-08-09 07:41:49 -0600

    Funciona

--- src/screens/IniciarSesion.js
+++ src/screens/IniciarSesion.js
@@ -67,7 +67,7 @@ export default function IniciarSesion({ navigation }) {
       formData.append('clave', contrasenia);
 
       // Enviar la solicitud de inicio de sesión al servidor
-      const response = await fetch(`${ip}/PTC_2024/api/services/public/cliente.php?action=logIn`, {
+      const response = await fetch(`${ip}/Sport_Development_3/api/services/public/cliente.php?action=logIn`, {
         method: 'POST',
         body: formData
       });
@@ -96,7 +96,6 @@ export default function IniciarSesion({ navigation }) {
   const irRecuperacion = () => {
     navigation.navigate('Recuperacion');
   };
-
   return (
     <View style={styles.container}>
       <Image

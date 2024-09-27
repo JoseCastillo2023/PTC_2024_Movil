f45f3a4981db13eb589fa77b4f2dd94e852d936e
alonso <20220664@ricaldone.edu.sv>
2024-09-27 10:45:56 -0600

    Arreglo de diseño 2

--- src/components/Inputs/Input.js
+++ src/components/Inputs/Input.js
@@ -1,34 +1,32 @@
-import { StyleSheet, TextInput } from "react-native";
-import React from "react";
+import { StyleSheet, TextInput } from 'react-native';
+import React from 'react';
 
 // Componente InputMultiline que recibe varias props para gestionar el valor, el texto y si es seguro
 export default function Input({ placeHolder, valor, contra, setTextChange }) {
   return (
     <TextInput
-      style={styles.Input}
-      placeholder={placeHolder}
-      value={valor}
-      onChangeText={setTextChange}
-      secureTextEntry={contra} // Controla si se debe ocultar el texto
-      placeholderTextColor="#16537E"
-    />
+    style={styles.Input}
+    placeholder={placeHolder}
+    value={valor}
+    onChangeText={setTextChange}
+    secureTextEntry={contra} // Controla si se debe ocultar el texto
+    placeholderTextColor="#623431"
+/>
   );
 }
 
 // Estilos para el componente InputMultiline
 const styles = StyleSheet.create({
   Input: {
-    backgroundColor: "#FFF",
-    color: "#16537E",
-    fontWeight: "500",
-    width: 320,
-    height: 45,
+    borderWidth: 1,
+    borderColor: '#623431', 
+    backgroundColor: '#FFF',
+    color: "#623431",
+    fontWeight: '500',
+    width: 350,
+    height: 45,  // Ajuste la altura para multiline
     borderRadius: 15,
     padding: 10,
-    marginVertical: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    borderWidth: 3,
-    borderColor: "#16537E",
+    marginVertical: 10
   },
 });
